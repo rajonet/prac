aefc14e5daf3e9009233b96cae8bc6525dd678e4
rajonet <rajonet25.7@gmail.com>
2025-03-20 14:28:36 +0530

    first commit

---
+++ Program1.java
@@ -0,0 +1,10 @@
+public class  Program1{
+    public static void main(String args[]){
+        int a = 10, b = 20, c = 0;
+        System.out.println("a = "+a+"\t"+"b = "+b);
+        c = b;
+        b = a;
+        a = c;
+        System.out.println("a = "+a+"\t"+"b = "+b);
+    }
+}
