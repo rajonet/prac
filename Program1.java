d3496d61cddb90356737ac3deb88802787b0bb06
rajonet <rajonet25.7@gmail.com>
2025-03-20 15:13:53 +0530

    fourth time

--- Program1.java
+++ Program1.java
@@ -1,12 +1,7 @@
 public class  Program1{
     public static void main(String args[]){
-        int a = 10, b = 20, c = 0;
-        System.out.println("a = "+a+"\t"+"b = "+b);
-        c = b;
-        b = a;
-        a = c;
-
-        System.out.println("a = "+a+"\t"+"b = "+b);
-        System.out.println("Raj");
+        for(int i = 10; i>=1; i--){
+            System.out.print(i+" ");
+        }
     }
 }
